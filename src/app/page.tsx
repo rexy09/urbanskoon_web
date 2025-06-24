c169a978800ca05a65dc72b240f3329428379c25
Fred Muju <fredmuju@gmail.com>
2025-06-24 12:57:23 +0300

    Add new company logos and icons
    - Added company7.png, company8.png, and company9.png to the public/company directory.
    - Introduced new SVG icons: chart-breakout-square.svg, gg_check-o.svg, tdesign_secured-filled.svg, and zap.svg to the public/icons directory.
    - Updated the visual assets to enhance the user interface and branding.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import {
   Text,
   SimpleGrid,
   Space,
+  Center,
+  Card,
 } from "@mantine/core";
 
 export default function Home() {
@@ -14,7 +16,7 @@ export default function Home() {
     <>
       <HeaderMenu />
 
-      <Container size="80%">
+      <Container size="90%">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
             <Text
@@ -55,7 +57,7 @@ export default function Home() {
             </Text>
             <Space h="xl" />
             <SimpleGrid cols={2}>
-              <Group>
+              <Group wrap="nowrap">
                 <Image
                   src="/icons/badge.svg"
                   alt="Icon"
@@ -66,7 +68,7 @@ export default function Home() {
                   20+ Years of Engineering Excellence
                 </Text>
               </Group>
-              <Group>
+              <Group wrap="nowrap">
                 <Image
                   src="/icons/badge.svg"
                   alt="Icon"
@@ -77,7 +79,7 @@ export default function Home() {
                   Multi-Disciplinary Expert Team
                 </Text>
               </Group>
-              <Group>
+              <Group wrap="nowrap">
                 <Image
                   src="/icons/badge.svg"
                   alt="Icon"
@@ -88,7 +90,7 @@ export default function Home() {
                   Proven Track Record Across Africa
                 </Text>
               </Group>
-              <Group>
+              <Group wrap="nowrap">
                 <Image
                   src="/icons/badge.svg"
                   alt="Icon"
@@ -103,15 +105,21 @@ export default function Home() {
 
             <Group mt={30}>
               <Button color="#FC6111">Explore our Services</Button>
-              <Button color="#FC6111" variant="subtle" c="#090909" rightSection={
-                <Image
-                  src="/icons/arrow_right.svg"
-                  alt="Icon"
-                  width={24}
-                  height={24}
-                />
-                
-              }>Start your Project </Button>
+              <Button
+                color="#FC6111"
+                variant="subtle"
+                c="#090909"
+                rightSection={
+                  <Image
+                    src="/icons/arrow_right.svg"
+                    alt="Icon"
+                    width={24}
+                    height={24}
+                  />
+                }
+              >
+                Start your Project{" "}
+              </Button>
             </Group>
           </div>
           <div>
@@ -124,7 +132,274 @@ export default function Home() {
             />
           </div>
         </div>
+        <Space h={"100px"} />
+        <Text ta={"center"} fw={500} fz={18} c={"#535862"}>
+          We’ve worked with some great startups
+        </Text>
+        <Space h={"lg"} />
+
+        <Group justify="center">
+          {Array.from({ length: 10 }).map((_, idx) => (
+            <Image
+              key={idx}
+              src={`/company/company${idx + 1}.png`}
+              alt="Icon"
+              width={26}
+              height={26}
+              style={{ width: "auto", height: "56px" }}
+            />
+          ))}
+        </Group>
+        <Space h={"100px"} />
+        <Text ta={"center"} fw={600} fz={42}>
+          Your Trusted Engineering Partner
+        </Text>
+        <Space h={"100px"} />
+        <Center>
+          <Text ta={"center"} fw={400} fz={23} c={"#535862"} maw={850}>
+            We offer our clients consulting, advisory, and specialized engineering
+            services with a focus on energy infrastructure, oil & gas facilities,
+            and industrial projects.
+          </Text>
+        </Center>
+        <Space h={"100px"} />
+
+        <SimpleGrid cols={{ base: 1, md: 3 }} spacing="xl">
+          <div>
+            <Center>
+              <div className="rounded-[11px] border border-[#E9EAEB] p-2.5 w-[56px]">
+                <Center>
+                  <Image
+                    src="/icons/gg_check-o.svg"
+                    alt="Engineering Icon"
+                    width={24}
+                    height={24}
+                  />
+                </Center>
+              </div>
+            </Center>
+            <Text fz={23} fw={600} mt="md" ta={"center"}>
+              Proven Expertise
+            </Text>
+            <Text fz={18} c="#535862" mt="sm" ta={"center"}>
+              Over two decades of engineering excellence across complex
+              infrastructure projects.
+            </Text>
+          </div>
+
+          <div>
+            <Center>
+              <div className="rounded-[11px] border border-[#E9EAEB] p-2.5 w-[56px]">
+                <Center>
+                  <Image
+                    src="/icons/zap.svg"
+                    alt="Engineering Icon"
+                    width={24}
+                    height={24}
+                  />
+                </Center>
+              </div>
+            </Center>
+            <Text fz={23} fw={600} mt="md" ta={"center"}>
+              Comprehensive Solutions
+            </Text>
+            <Text fz={18} c="#535862" mt="sm" ta={"center"}>
+              From concept to completion, we provide end-to-end engineering
+              consulting services.
+            </Text>
+          </div>
+          <div>
+            <Center>
+              <div className="rounded-[11px] border border-[#E9EAEB] p-2.5 w-[56px]">
+                <Center>
+                  <Image
+                    src="/icons/chart-breakout-square.svg"
+                    alt="Engineering Icon"
+                    width={24}
+                    height={24}
+                  />
+                </Center>
+              </div>
+            </Center>
+            <Text fz={23} fw={600} mt="md" ta={"center"}>
+              Innovation Driven
+            </Text>
+            <Text fz={18} c="#535862" mt="sm" ta={"center"}>
+              Leveraging cutting-edge technology and industry best practices for
+              optimal results.
+            </Text>
+          </div>
+        </SimpleGrid>
+        <Space h={"100px"} />
       </Container>
+      <Container size="lg">
+        <Text ta={"center"} fw={600} fz={42}>
+          Industries We Serve
+        </Text>
+        {/* <Space h={"md"} /> */}
+        <Center>
+          <Text ta={"center"} fw={400} fz={23} c={"#535862"} maw={850}>
+            Our expertise spans across critical energy and infrastructure sectors, delivering specialized solutions for each industry's unique challenges.
+          </Text>
+        </Center>
+        <Space h={"100px"} />
+        <SimpleGrid cols={{ base: 1, md: 2 }} spacing="xl">
+
+          <Card shadow="sm" padding="lg" radius="24px" style={{ border: "0.5px solid #090909" }}>
+            <Group wrap="nowrap" >
+              <div style={{ borderRadius: "50%", padding: "16px", backgroundColor: "#FC611138" }}>
+                <Image
+                  src="/icons/tdesign_secured-filled.svg"
+                  alt="secured"
+                  width={42.39}
+                  height={42.39}
+                />
+              </div>
+              <div>
+                <Group wrap="nowrap" justify="space-between" >
+                  <Text fz={19} fw={700} c="#090909">
+                    Oil & Gas
+                  </Text>
+                  <div style={{ borderRadius: "10px", padding: "5px", backgroundColor: "#FC611138" }}>
+                    <Text fz={14} fw={700} c="#090909">
+                      150+ Projects
+                    </Text>
+                  </div>
+
+                </Group>
+
+                <Text fz={19} fw={400} c="#4A4A4C" mt="sm">
+                  Upstream, midstream, and downstream operations including refineries, processing plants, and distribution systems.
+                </Text>
+              </div>
+
+            </Group>
+          </Card>
+          <Card shadow="sm" padding="lg" radius="24px" style={{ border: "0.5px solid #090909" }}>
+            <Group wrap="nowrap" >
+              <div style={{ borderRadius: "50%", padding: "16px", backgroundColor: "#FC611138" }}>
+                <Image
+                  src="/icons/tdesign_secured-filled.svg"
+                  alt="secured"
+                  width={42.39}
+                  height={42.39}
+                />
+              </div>
+              <div>
+                <Group wrap="nowrap" justify="space-between" >
+                  <Text fz={19} fw={700} c="#090909">
+                    Pipeline Infrastructure
+                  </Text>
+                  <div style={{ borderRadius: "10px", padding: "5px", backgroundColor: "#FC611138" }}>
+                    <Text fz={14} fw={700} c="#090909">
+                      200+ Miles
+                    </Text>
+                  </div>
+
+                </Group>
+
+                <Text fz={19} fw={400} c="#4A4A4C" mt="sm">
+                  Design and implementation of oil, gas, and water pipeline systems with advanced monitoring and control systems.
+                </Text>
+              </div>
+
+            </Group>
+          </Card>
+          <Card shadow="sm" padding="lg" radius="24px" style={{ border: "0.5px solid #090909" }}>
+            <Group wrap="nowrap" >
+              <div style={{ borderRadius: "50%", padding: "16px", backgroundColor: "#FC611138" }}>
+                <Image
+                  src="/icons/tdesign_secured-filled.svg"
+                  alt="secured"
+                  width={42.39}
+                  height={42.39}
+                />
+              </div>
+              <div>
+                <Group wrap="nowrap" justify="space-between" >
+                  <Text fz={19} fw={700} c="#090909">
+                    CNG/LPG Systems
+                  </Text>
+                  <div style={{ borderRadius: "10px", padding: "5px", backgroundColor: "#FC611138" }}>
+                    <Text fz={14} fw={700} c="#090909">
+                      75+ Facilities
+                    </Text>
+                  </div>
+
+                </Group>
+
+                <Text fz={19} fw={400} c="#4A4A4C" mt="sm">
+                  Compressed and liquefied gas storage, transportation, and distribution infrastructure solutions.
+                </Text>
+              </div>
+
+            </Group>
+          </Card>
+          <Card shadow="sm" padding="lg" radius="24px" style={{ border: "0.5px solid #090909" }}>
+            <Group wrap="nowrap" >
+              <div style={{ borderRadius: "50%", padding: "16px", backgroundColor: "#FC611138" }}>
+                <Image
+                  src="/icons/tdesign_secured-filled.svg"
+                  alt="secured"
+                  width={42.39}
+                  height={42.39}
+                />
+              </div>
+              <div>
+                <Group wrap="nowrap" justify="space-between" >
+                  <Text fz={19} fw={700} c="#090909">
+                    Infrastructure
+                  </Text>
+                  <div style={{ borderRadius: "10px", padding: "5px", backgroundColor: "#FC611138" }}>
+                    <Text fz={14} fw={700} c="#090909">
+                      100+ Facilities
+                    </Text>
+                  </div>
+
+                </Group>
+
+                <Text fz={19} fw={400} c="#4A4A4C" mt="sm">
+                  Critical infrastructure development including power generation, water treatment, and industrial facilities.
+                </Text>
+              </div>
+
+            </Group>
+          </Card>
+        </SimpleGrid>
+      </Container>
+
+      <Space h={"100px"} />
+      <Container size="xl">
+        <div style={{ backgroundColor: "#FC6111", maxHeight:"400px", width:"100%"}}>
+          <Space h={"100px"} />
+
+          <Text ta={"center"} fw={700} fz={36} c={"white"} style={{fontFamily:"Satoshi"}}>
+            Ready to Start Your Next Project?
+          </Text>
+          {/* <Space h={"md"} /> */}
+          <Center>
+            <Text ta={"center"} fw={400} fz={20} c={"white"} maw={700} style={{fontFamily:"Satoshi"}}>
+              Let's discuss how MOKKO can help you achieve your engineering goals with our expertise and innovative solutions.
+            </Text>
+          </Center>
+          <Space h={"50px"} />
+          <Group  justify="center">
+            <Button color="#090909" c={"white"}>Get in Touch</Button>
+            <Button
+              color="white"
+              // variant="subtle"
+              c="#FC6111"
+             
+            >
+              Learn about us 
+            </Button>
+          </Group>
+          <Space h={"100px"} />
+
+        </div>
+
+      </Container>
+      <Space h={"100px"} />
     </>
   );
 }
